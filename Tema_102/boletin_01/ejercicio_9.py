00cdf34a71888c54baf8b7f54cb2365472186e3a
AuxXiii <115449975+AuxXis@users.noreply.github.com>
2022-11-22 19:16:05 +0100

    Delete Tema_102/boletin_01 directory

--- Tema_102/boletin_01/ejercicio_9.py
+++
@@ -1,7 +0,0 @@
-'''
-Desarrolla un programa que a partir de una lista de números y pedir un entero k, realice la llamada a tres 
-funciones: 
-a) para devolver una lista de números con los menores de k. 
-b) otra con los mayores que k.
-c) otra con aquellos que son múltiplos de k.
-'''
